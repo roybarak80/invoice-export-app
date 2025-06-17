54f49de255fb05af9f8d44042ef969e3e633643a
Roy Barak <barakroy@gmail.com>
2025-06-17 08:47:19 +0300

    create-docker:added-docker-files

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
+});
+
 server.use(middlewares);
 server.use(
   jsonServer.rewriter({
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://api.example.com'
+  apiBaseUrl: 'http://localhost:3000/api'
 };
